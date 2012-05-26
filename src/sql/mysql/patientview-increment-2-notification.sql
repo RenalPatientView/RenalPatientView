42cd93ec86ecfe8ccfe336580634779cab0ed505
Jeff Snyder <jeff@caffeinated.me.uk>
2012-05-26 18:34:09 +0100

    Revert "Hibernate config and SQL for a notification date table"
    moved onto branch 'notification'
    
    This reverts commit 11dc0896839a38d533d30b80563ecf0e6bc7d62e.

--- src/sql/mysql/patientview-increment-2-notification.sql
+++
@@ -1,8 +0,0 @@
--- Create table last_notification
-DROP TABLE IF EXISTS `notification`;
-CREATE TABLE `notification` (
-  `name` varchar(100) NOT NULL,
-  `lastnotification` datetime NOT NULL,
-  PRIMARY KEY (`name`)
-);
-
